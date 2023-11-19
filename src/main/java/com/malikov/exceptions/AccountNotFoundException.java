371a98a996091da723e88335d85e4da5c5b38442
TooWorthless <91129167+TooWorthless@users.noreply.github.com>
2023-11-19 17:08:27 +0300

    Added 3 exeptions

---
+++ src/main/java/com/malikov/exceptions/AccountNotFoundException.java
@@ -0,0 +1,7 @@
+package com.malikov.exceptions;
+
+public class AccountNotFoundException extends Exception {
+    public AccountNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/malikov/exceptions/NegativeAmountException.java
@@ -0,0 +1,7 @@
+package com.malikov.exceptions;
+
+public class NegativeAmountException extends Exception {
+    public NegativeAmountException(String message) {
+        super(message);
+    }
+}
