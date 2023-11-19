81653cf77aba95a8171fd2b5869d096a45a3725e
TooWorthless <91129167+TooWorthless@users.noreply.github.com>
2023-11-19 17:17:44 +0300

    Created classes Bank and BankAccount

---
+++ src/main/java/com/malikov/bank/Bank.java
@@ -0,0 +1,42 @@
+package com.malikov.bank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.malikov.exceptions.AccountNotFoundException;
+import com.malikov.exceptions.InsufficientFundsException;
+import com.malikov.exceptions.NegativeAmountException;
+import com.malikov.bankAccount.BankAccount;
+
+public class Bank {
+    private List<BankAccount> accounts;
+
+    public Bank() {
+        this.accounts = new ArrayList<>();
+    }
+
+    public BankAccount createAccount(String accountName, double initialDeposit) {
+        int accountNumber = accounts.size() + 1;
+        BankAccount newAccount = new BankAccount(accountNumber, accountName, initialDeposit);
+        accounts.add(newAccount);
+        return newAccount;
+    }
+
+    public BankAccount findAccount(int accountNumber) throws AccountNotFoundException {
+        for (BankAccount account : accounts) {
+            if (account.getAccountNumber() == accountNumber) {
+                return account;
+            }
+        }
+        throw new AccountNotFoundException("Account not found with account number: " + accountNumber);
+    }
+
+    public void transferMoney(int fromAccountNumber, int toAccountNumber, double amount)
+            throws AccountNotFoundException, InsufficientFundsException, NegativeAmountException {
+        BankAccount fromAccount = findAccount(fromAccountNumber);
+        BankAccount toAccount = findAccount(toAccountNumber);
+
+        fromAccount.withdraw(amount);
+        toAccount.deposit(amount);
+    }
+}
